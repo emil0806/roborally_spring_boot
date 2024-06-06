ea07615511ce116339c1898ee61e97b84d3a23c7
Emil Leonhard Lauritzen <s231331@dtu.dk>
2024-06-06 20:20:35 +0200

    initial commit

---
+++ src/main/java/com/example/roborally/RoborallyApplication.java
@@ -0,0 +1,13 @@
+package com.example.roborally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoborallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoborallyApplication.class, args);
+	}
+
+}
